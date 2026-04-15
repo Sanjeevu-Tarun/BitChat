22ed1c31603f728ea43b12b0153c445bef6b1cad
test2 <sanjeevutarun2005@gmail.com>
2026-04-15 14:27:30 +0530

    updated

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,11 @@ pluginManagement {
     }
 }
 
+// Add this block here
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
